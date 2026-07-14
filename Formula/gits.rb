88292366667f4f0600855828930a3b119d77f2d5
yuyuelin <yuelin.yu@clobotics.com>
2026-07-14 10:44:30 +0800

    Fix redundant formula version

--- Formula/gits.rb
+++ Formula/gits.rb
@@ -2,7 +2,6 @@ class Gits < Formula
   desc "Project-scoped Git submodule workflow with a shared repository cache"
   homepage "https://github.com/leo1394/homebrew-gits"
   url "https://raw.githubusercontent.com/leo1394/homebrew-gits/v0.1.0/bin/gits", using: :nounzip
-  version "0.1.0"
   sha256 "9c99ed0a0c9e86221379a7ef20e7a5e392bddb78210aadfe662f876d7b4384e0"
   license "MIT"
   head "https://github.com/leo1394/homebrew-gits.git", branch: "main"
